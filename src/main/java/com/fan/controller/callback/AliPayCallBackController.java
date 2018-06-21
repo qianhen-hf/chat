0a80fe5d4f73b7757fc5a116026ea164a31e5cbe
qianhen-hf <928923490@qq.com>
2018-06-22 03:21:21 +0800

    更换springcloud版本 短信成功

--- src/main/java/com/fan/controller/callback/AliPayCallBackController.java
+++ src/main/java/com/fan/controller/callback/AliPayCallBackController.java
@@ -1,6 +1,5 @@
 package com.fan.controller.callback;
 
-import com.alipay.api.internal.util.AlipaySignature;
 import com.fan.Exception.VRabbitException;
 import com.fan.Exception.VRabbitUserErrors;
 import com.fan.po.AliPay;
@@ -13,9 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 /**
  * java类简单作用描述
