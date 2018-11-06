98793544323b999c7866f545ef152cab88923aeb
qianhen-hf <928923490@qq.com>
2018-11-06 09:31:28 +0800

    礼物

--- src/main/java/com/fan/controller/IndexPicController.java
+++ src/main/java/com/fan/controller/IndexPicController.java
@@ -1,6 +1,13 @@
 package com.fan.controller;
 
+import com.fan.po.IndexPic;
+import com.fan.service.IndexPicService;
+import com.fan.vo.ResponseResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,6 +22,27 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version: 1.0
  */
 @RestController
+@Api(value = "首页图片", tags = {"首页图片"})
+@RequestMapping("vRabbit/index")
 public class IndexPicController {
-    
+    @Autowired
+    IndexPicService indexPicService;
+
+
+    @ApiOperation(value = "查询所有首页图片")
+    @RequestMapping("getIndexPics")
+    public ResponseResult getIndexPics() {
+        ResponseResult responseResult = new ResponseResult();
+        responseResult.setData(indexPicService.getIndexPics());
+        return responseResult;
+    }
+
+    @ApiOperation(value = "查询所有在线繁忙的主播")
+    @RequestMapping("getIndexCustomers")
+    public ResponseResult getIndexCustomers() {
+        ResponseResult responseResult = new ResponseResult();
+        responseResult.setData(indexPicService.getIndexCustomers());
+        return responseResult;
+
+    }
 }
